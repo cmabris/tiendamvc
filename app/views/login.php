8a7a07120a7b34e1ed9e7e910fe6cb51450693fd
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-09-27 21:11:57 +0200

    Fin paso 14

--- app/views/login.php
+++ app/views/login.php
@@ -6,23 +6,47 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </head>
 <body>
-<form>
-    <table>
-        <tr>
-            <td>Usuario:</td>
-            <td><input type="text" name="user"></td>
-        </tr>
-        <tr>
-            <td>Clave de acceso:</td>
-            <td><input type="password" name="password"></td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td><input type="submit" value="Enviar"></td>
-        </tr>
-    </table>
-</form>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a href="index.php" class="navbar-brand">Tienda</a>
+</nav>
+<div class="container-fluid">
+    <div class="row content">
+        <div class="col-sm-2">
+
+        </div>
+        <div class="col-sm-8">
+            <form>
+                <table>
+                    <tr>
+                        <td>Usuario:</td>
+                        <td><input type="text" name="user"></td>
+                    </tr>
+                    <tr>
+                        <td>Clave de acceso:</td>
+                        <td><input type="password" name="password"></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td><input type="submit" value="Enviar"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <div class="col-sm-2">
+
+        </div>
+    </div>
+</div>
 </body>
 </html>
