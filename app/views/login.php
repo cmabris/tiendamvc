72f5ef4b441196c46706b6e49f0eba6a3d9f991f
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-09-27 20:30:47 +0200

    Fin paso 12

---
+++ app/views/login.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<form>
+    <table>
+        <tr>
+            <td>Usuario:</td>
+            <td><input type="text" name="user"></td>
+        </tr>
+        <tr>
+            <td>Clave de acceso:</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" value="Enviar"></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
