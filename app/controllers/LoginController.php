562dbd61a66cbc1dfb4c9e1b4aabfa58fdc35cd4
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-09-28 20:49:12 +0200

    Paso 19

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -26,11 +26,86 @@ class LoginController extends Controller
 
     public function registro()
     {
-        $data = [
-            'titulo' => 'Registro',
-            'menu'   => false,
-        ];
+        $errors = [];
+        $dataForm = [];
 
-        $this->view('register', $data);
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Procesamos la información recibida del formulario
+            $firstName = $_POST['first_name'] ?? '';
+            $lastName1 = $_POST['last_name_1'] ?? '';
+            $lastName2 = $_POST['last_name_2'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password1 = $_POST['password'] ?? '';
+            $password2 = $_POST['password2'] ?? '';
+            $address = $_POST['address'] ?? '';
+            $city = $_POST['city'] ?? '';
+            $state = $_POST['state'] ?? '';
+            $postcode = $_POST['postcode'] ?? '';
+            $country = $_POST['country'] ?? '';
+
+            $dataForm = [
+                'firstName' => $firstName,
+                'lastName1' => $lastName1,
+                'lastName2' => $lastName2,
+                'email' 	=> $email,
+                'password'  => $password1,
+                'address'	=> $address,
+                'city'		=> $city,
+                'state'		=> $state,
+                'postcode'	=> $postcode,
+                'country'	=> $country
+            ];
+
+            if ($firstName == '') {
+                array_push($errors, 'El nombre es requerido');
+            }
+            if ($lastName1 == '') {
+                array_push($errors, 'El primer apellido es requerido');
+            }
+            if ($lastName2 == '') {
+                array_push($errors, 'El segundo apellido es requerido');
+            }
+            if ($email == '') {
+                array_push($errors, 'El email es requerido');
+            }
+            if ($password1 == '') {
+                array_push($errors, 'La contraseña es requerido');
+            }
+            if ($password2 == '') {
+                array_push($errors, 'Repetir contraseña es requerido');
+            }
+            if ($address == '') {
+                array_push($errors, 'La dirección es requerida');
+            }
+            if ($city == '') {
+                array_push($errors, 'La ciudad es requerida');
+            }
+            if ($state == '') {
+                array_push($errors, 'La provincia es requerida');
+            }
+            if ($postcode == '') {
+                array_push($errors, 'El código postal es requerido');
+            }
+            if ($country == '') {
+                array_push($errors, 'El país es requerido');
+            }
+            if ($password1 != $password2) {
+                array_push($errors, 'Las contraseñas deben ser iguales');
+            }
+
+            if (count($errors) == 0) {
+                print 'Pasamos a dar de alta al usuario en la BD';
+            } else {
+                var_dump($errors);
+            }
+        } else {
+            // Mostramos el formulario
+            $data = [
+                'titulo' => 'Registro',
+                'menu'   => false,
+            ];
+
+            $this->view('register', $data);
+        }
     }
 }
