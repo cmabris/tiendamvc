006cb0e14a2c5f2eeb9e6f80430fcaf2a23631f9
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-10-05 19:59:49 +0200

    Paso 38

--- app/controllers/AdminUserController.php
+++ app/controllers/AdminUserController.php
@@ -23,14 +23,65 @@ class AdminUserController extends Controller
 
     public function create()
     {
-        $data = [
-            'titulo' => 'Administración de Usuarios - Alta',
-            'menu' => false,
-            'admin' => true,
-            'data' => [],
-        ];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $this->view('admin/users/create', $data);
+            $errors = [];
+
+            $name = $_POST['name'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password1 = $_POST['password1'] ?? '';
+            $password2 = $_POST['password2'] ?? '';
+
+            $dataForm = [
+                'name' => $name,
+                'email' => $email,
+                'password1' => $password1,
+            ];
+
+            if (empty($name)) {
+                array_push($errors, 'El nombre de usuario es requerido');
+            }
+            if (empty($email)) {
+                array_push($errors, 'El correo electrónico de usuario es requerido');
+            }
+            if (empty($password1)) {
+                array_push($errors, 'La clave de acceso es requerida');
+            }
+            if (empty($password2)) {
+                array_push($errors, 'La verificación de clave es requerida');
+            }
+            if ($password1 != $password2) {
+                array_push($errors, 'Las claves no coinciden');
+            }
+
+            if ( ! $errors) {
+                // Añadir en DB
+            } else {
+
+                $data = [
+                    'titulo' => 'Administración de Usuarios - Alta',
+                    'menu' => false,
+                    'admin' => true,
+                    'errors' => $errors,
+                    'data' => $dataForm,
+                ];
+
+                $this->view('admin/users/create', $data);
+
+            }
+
+        } else {
+
+            $data = [
+                'titulo' => 'Administración de Usuarios - Alta',
+                'menu' => false,
+                'admin' => true,
+                'data' => [],
+            ];
+
+            $this->view('admin/users/create', $data);
+
+        }
     }
 
     public function update()
