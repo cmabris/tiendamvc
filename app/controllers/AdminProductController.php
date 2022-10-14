2a406e765efcbf7162065477f3e9e72db72c9f1d
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-10-14 20:16:50 +0200

    Paso 60

--- app/controllers/AdminProductController.php
+++ app/controllers/AdminProductController.php
@@ -41,6 +41,49 @@ class AdminProductController extends Controller
         $status = $this->model->getConfig('productStatus');
         $catalogue = $this->model->getCatalogue();
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            
+            $type = $_POST['type'] ?? '';
+            $name = $_POST['name'] ?? '';
+            $description = $_POST['description'] ?? '';
+            $price = $_POST['price'] ?? '';
+            $discount = $_POST['discount'] ?? '';
+            $send = $_POST['send'] ?? '';
+            $image = $_FILES['image']['name'];
+            $published = $_POST['published'] ?? '';
+            $relation1 = $_POST['relation1'] != '' ? $_POST['relation1'] : 0;
+            $relation2 = $_POST['relation2'] != '' ? $_POST['relation2'] : 0;
+            $relation3 = $_POST['relation3'] != '' ? $_POST['relation3'] : 0;
+            $mostSold = $_POST['mostSold'] ?? '';
+            $new =$_POST['new'] ?? '';
+            $status = $_POST['status'] ?? '';
+            //Books
+            $author = $_POST['author'] ?? '';
+            $publisher = $_POST['publisher'] ?? '';
+            $pages = $_POST['pages'] ?? '';
+            //Courses
+            $people = $_POST['people'] ?? '';
+            $objetives = $_POST['objetives'] ?? '';
+            $necesites = $_POST['necesites'] ?? '';
+
+
+            // Validamos la información
+
+            // Creamos el array de datos
+
+            if ( ! $errors ) {
+
+                // Enviamos la información al modelo
+
+                if ( ! $errors ) {
+
+                    // Redirigimos al index de productos
+
+                }
+            }
+
+        }
+
         $data = [
             'titulo' => 'Administración de Productos - Alta',
             'menu' => false,
