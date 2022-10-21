e4cd0a8210dedc1ca3ad0b3d7df0fd0de396f496
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-10-21 20:40:02 +0200

    Paso 93

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -9,8 +9,20 @@ class CartController extends Controller
         $this->model = $this->model('Cart');
     }
 
+    public function index()
+    {
+        
+    }
+
     public function addProduct($product_id, $user_id)
     {
-        var_dump($product_id, $user_id);
+        $errors = [];
+
+        if ($this->model->verifyProduct($product_id, $user_id) == false) {
+            if ($this->model->addProduct($product_id, $user_id) == false) {
+                array_push($errors, 'Error al insertar el producto en el carrito');
+            }
+        }
+        $this->index();
     }
 }
--- app/models/Cart.php
+++ app/models/Cart.php
@@ -8,4 +8,40 @@ class Cart
     {
         $this->db = Mysqldb::getInstance()->getDatabase();
     }
+
+    public function verifyProduct($product_id, $user_id)
+    {
+        $sql = 'SELECT * FROM carts WHERE product_id=:product_id AND user_id=:user_id';
+        $query = $this->db->prepare($sql);
+        $params = [
+            ':product_id' => $product_id,
+            ':user_id' => $user_id,
+        ];
+        $query->execute($params);
+
+        return $query->rowCount();
+    }
+
+    public function addProduct($product_id, $user_id)
+    {
+        $sql = 'SELECT * FROM products WHERE id=:id';
+        $query = $this->db->prepare($sql);
+        $query->execute([':id' => $product_id]);
+        $product = $query->fetch(PDO::FETCH_OBJ);
+
+        $sql2 = 'INSERT INTO carts(state, user_id, product_id, quantity, discount, send, date)
+                 VALUES (:state, :user_id, :product_id, :quantity, :discount, :send, :date)';
+        $query2 = $this->db->prepare($sql2);
+        $params2 = [
+            ':state' => 0,
+            ':user_id' => $user_id,
+            ':product_id' => $product_id,
+            ':quantity' => 1,
+            ':discount' => $product->discount,
+            ':send' => $product->send,
+            ':date' => date('Y-m-d H:i:s'),
+        ];
+        $query2->execute($params2);
+        return $query2->rowCount();
+    }
 }
